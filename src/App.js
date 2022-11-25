cb2428c8db47db1fb5ea15a775ce4c02e0e7285d
MariamK <102153226+MariamKakushadze@users.noreply.github.com>
2022-11-25 22:24:41 +0400

    finished project

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import Search from "./components/search";
 import "./App.css";
 import UserInfo from "./components/useInfo";
 import React, { useState } from "react";
+
 function App() {
   const [theme, setTheme] = useState(false);
+  const [searchUser, setSearchUser] = useState("octocat");
+  const [result, setResult] = useState(true);
 
   return (
     <div className="App" style={{ background: theme ? "#F2F2F2" : "#141D2F" }}>
       <div className="container">
         <Header theme={theme} setTheme={setTheme} />
-        <Search theme={theme} setTheme={setTheme} />
-        <UserInfo theme={theme} setTheme={setTheme} />
+        <Search
+          theme={theme}
+          setSearchUser={setSearchUser}
+          searchUser={searchUser}
+          result={result}
+        />
+        <UserInfo
+          theme={theme}
+          searchUser={searchUser}
+          result={result}
+          setResult={setResult}
+        />
       </div>
     </div>
   );
--- src/components/search.js
+++ src/components/search.js
@@ -1,20 +1,27 @@
 import "./search.css";
 import searchIcon from "../assets/icon-search.svg";
 
-export default function Search({ theme }) {
+export default function Search({ theme, setSearchUser, result }) {
+  const searchHandler = (e) => {
+    e.preventDefault();
+    setSearchUser(e.target.search.value);
+  };
   return (
     <>
-      <form>
+      <form onSubmit={searchHandler} autoComplete="off">
         <label htmlFor="search">
           <img src={searchIcon} alt="search" />
           <input
             type="search"
             id="search"
             placeholder="Search GitHub username..."
-            style={{ background: theme ? "#FFF" : "#1e2a47" }}
+            style={{
+              background: theme ? "#FFF" : "#1e2a47",
+              color: theme ? "#1e2a47" : "#FFF",
+            }}
           />
         </label>
-        {/* <span className="error">no result</span> */}
+        <span className="error">{!result ? "no result" : ""}</span>
         <button type="submit">Search</button>
       </form>
     </>
